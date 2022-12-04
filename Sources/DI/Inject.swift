d1f771d74620fb9bbf461c52e287171ee5e1b9b7
Giorgi Kratsashvili <gkrat17@freeuni.edu.ge>
2022-12-04 19:14:16 +0400

    change sequence of properties

--- Sources/DI/Inject.swift
+++ Sources/DI/Inject.swift
@@ -9,6 +9,7 @@ public class Inject<T> {
     public var container: DependencyContainer
     public var id: Dependency.ID
     public var storage: Bool
+    public var storageValue: T?
     public var wrappedValue: T {
         if storage {
             if let value = storageValue {
@@ -21,7 +22,6 @@ public class Inject<T> {
         storageValue = nil
         return container.resolve(id: id)
     }
-    public var storageValue: T?
 
     public init(container: DependencyContainer = .shared, id: Dependency.ID? = nil, storage: Bool = true) {
         self.container = container
